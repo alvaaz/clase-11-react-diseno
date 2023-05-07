de7630ca375a9dea7202853730cafa642f289096
Álvaro Goede <alvaro@goede.cl>
2023-05-07 18:38:44 -0400

    example 1 updated

--- example-1/src/App.tsx
+++ example-1/src/App.tsx
@@ -1,7 +1,45 @@
+import { Card } from "./components/card";
+
+const data = [
+  {
+    title: "2010x Mix",
+    description: "Jacob Collier, Daryl Hall & John Oates, Jason Mraz",
+    imageUrl: "/ed-sheeran.png",
+    color: "rgb(254 205 211)",
+  },
+  {
+    title: "2010x Mix",
+    description: "Jacob Collier, Daryl Hall & John Oates, Jason Mraz",
+    imageUrl: "/los-tres.png",
+    color: "rgb(94 234 212)",
+  },
+  {
+    title: "2010x Mix",
+    description: "Jacob Collier, Daryl Hall & John Oates, Jason Mraz",
+    imageUrl: "/alejandro.png",
+    color: "rgb(125 211 252)",
+  },
+  {
+    title: "2010x Mix",
+    description: "Jacob Collier, Daryl Hall & John Oates, Jason Mraz",
+    imageUrl: "/john-mayer.png",
+    color: "rgb(217 249 157)",
+  },
+];
+
 function App() {
   return (
     <>
-      <h1>Example 1</h1>
+      <div className="flex gap-4">
+        {data.map((item) => (
+          <Card
+            title={item.title}
+            description={item.description}
+            imageUrl={item.imageUrl}
+            color={item.color}
+          />
+        ))}
+      </div>
     </>
   );
 }
---
+++ example-1/src/components/card/index.tsx
@@ -0,0 +1,46 @@
+type Props = {
+  title: string;
+  description: string;
+  imageUrl: string;
+  color?: string;
+};
+
+export function Card({ imageUrl, title, description, color = "red" }: Props) {
+  return (
+    <figure className="bg-[#181818] hover:bg-[#282828] rounded-lg overflow-hidden p-4 cursor-pointer transition-all group/card">
+      <div className="rounded-lg overflow-hidden mb-4 relative">
+        <div
+          style={{
+            backgroundImage: `url('${imageUrl}')`,
+            borderBottomColor: color,
+          }}
+          className={`bg-cover aspect-square relative border-b-4 bg-center after:bg-gradient-to-b after:absolute after:from-transparent after:to-black after:top-0 after:left-0 after:right-0 after:bottom-0 after:w-full after:h-full`}
+        >
+          <h1
+            style={{ borderLeftColor: color }}
+            className={`text-white drop-shadow-md border-l-4 pl-4 font-bold absolute bottom-4 z-10`}
+          >
+            {title}
+          </h1>
+        </div>
+        <svg
+          width="14"
+          height="17"
+          viewBox="0 0 14 17"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+          className="absolute right-4 bottom-4 z-20 bg-green-500 rounded-full w-12 h-12 p-4 opacity-0 group-hover/card:opacity-100 transition-all hover:bg-green-400 hover:scale-110"
+        >
+          <path
+            d="M0.5 15.2768V1.83167C0.5 1.0405 1.37525 0.562658 2.04076 0.990487L13.1132 8.10851C13.7447 8.51445 13.7204 9.44549 13.0686 9.81793L1.99614 16.1451C1.32948 16.526 0.5 16.0446 0.5 15.2768Z"
+            fill="black"
+          />
+        </svg>
+      </div>
+      <figcaption>
+        <h2 className="font-bold text-white mb-3">{title}</h2>
+        <p className="text-gray-300">{description}</p>
+      </figcaption>
+    </figure>
+  );
+}
